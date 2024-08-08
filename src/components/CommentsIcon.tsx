03650cf0866fa89589dfc7aa6579528773910219
soufianeoualla <soufianeoualla0@gmail.com>
2024-08-08 15:16:05 +0100

    fix type issue

--- src/components/CommentsIcon.tsx
+++ src/components/CommentsIcon.tsx
@@ -1,15 +1,18 @@
 import { MessageCircle } from "lucide-react";
 import { cn } from "~/lib/utils";
-
-export const CommentsIcon = ({
-  commentsLength,
-  className
-}: {
+type Props = {
   commentsLength: number;
-  className: string;
-}) => {
+  className?: string;
+};
+
+export const CommentsIcon = ({ commentsLength, className }: Props) => {
   return (
-    <div className={cn("flex items-center gap-x-2 mobile:absolute mobile:bottom-4 mobile:right-4",className)}>
+    <div
+      className={cn(
+        "flex items-center gap-x-2 mobile:absolute mobile:bottom-4 mobile:right-4",
+        className,
+      )}
+    >
       <MessageCircle className="fill-slate-200 text-slate-200" />
       <b className="text-dark-blue">{commentsLength}</b>
     </div>
